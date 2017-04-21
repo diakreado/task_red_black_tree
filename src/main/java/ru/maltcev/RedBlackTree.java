86f7c6c67750ae7687b6e31370007c7f3e9b5bbf
Михаил Мальцев <mikle9997@mail.ru>
2017-04-21 09:42:33 +0300

    Исправил замечаня
    Теперь все работает, как запланировано

--- src/main/java/ru/maltcev/RedBlackTree.java
+++ src/main/java/ru/maltcev/RedBlackTree.java
@@ -2,43 +2,36 @@ package ru.maltcev;
 
 public class RedBlackTree {
 
+    /**
+     * @param root корневой узел, от которого начинается проверка
+     * @return проверяет сбалансированость дерева, идет проверка на симметричность, так как, по-момему, именно это
+     *          обозначает сбалансированость
+     */
     public static boolean checkLengthOfBranch(RBNode root) {
 
         if (root.getLeft() == null)
             return root.getRight() == null;
 
-        lengthDetermination(root);
+        System.out.println(lengthDetermination(root.getLeft()) + " " + lengthDetermination(root.getRight()));
 
-        return isConditionFulfilled;
+        return lengthDetermination(root) != -1;
     }
-
-    private static boolean isConditionFulfilled = true;
 
     private static int lengthDetermination(RBNode node) {
 
         int currentLeft = 0;
         int currentRight = 0;
+        int current = node.isRed()?  0 : 1;
 
-        if (node.getLeft() != null) {
-
+        if (node.getLeft() != null)
             currentLeft = lengthDetermination(node.getLeft());
 
-            if (!node.getLeft().isRed())
-                currentLeft++;
-        }
-
-        if (node.getRight() != null) {
-
+        if (node.getRight() != null)
             currentRight = lengthDetermination(node.getRight());
 
-            if (!node.getRight().isRed())
-                currentRight++;
-        }
+        if(currentLeft == -1 || currentLeft != currentRight)     //(currentLeft == -1)  уже не требуется
+            return -1;
 
-        if(currentLeft != currentRight)
-            isConditionFulfilled = false;
-
-        return currentLeft + currentRight;
+        return currentLeft + currentRight + current;
     }
-
 }
--- src/test/java/ru/maltcev/RedBlackTreeTest.java
+++ src/test/java/ru/maltcev/RedBlackTreeTest.java
@@ -35,7 +35,45 @@ public class RedBlackTreeTest {
         assertFalse(checkLengthOfBranch(root));
     }
 
-    class RBNodeTest<T> implements RBNode<T> {
+    @Test
+    public void test1() {
+
+        RBNodeTest<Integer> root  = new RBNodeTest<>(10);
+        RBNodeTest<Integer> node6 = new RBNodeTest<>(6);
+        RBNodeTest<Integer> node17 = new RBNodeTest<>(17,true);
+        RBNodeTest<Integer> node13 = new RBNodeTest<>(13,true);
+        RBNodeTest<Integer> node3 = new RBNodeTest<>(3,true);
+        RBNodeTest<Integer> node8 = new RBNodeTest<>(8,true);
+        RBNodeTest<Integer> node15 = new RBNodeTest<>(15);
+        RBNodeTest<Integer> node2 = new RBNodeTest<>(2);
+        RBNodeTest<Integer> node4 = new RBNodeTest<>(4);
+        RBNodeTest<Integer> node7 = new RBNodeTest<>(7);
+        RBNodeTest<Integer> node9 = new RBNodeTest<>(9);
+        RBNodeTest<Integer> node12 = new RBNodeTest<>(12);
+        RBNodeTest<Integer> node14 = new RBNodeTest<>(14);
+        RBNodeTest<Integer> node16 = new RBNodeTest<>(16);
+        RBNodeTest<Integer> node18 = new RBNodeTest<>(18);
+
+        root.setLeft(node6);
+        node6.setRight(node8);
+        node6.setLeft(node3);
+        node3.setRight(node4);
+        node3.setLeft(node2);
+        node8.setRight(node9);
+        node8.setLeft(node7);
+
+        root.setRight(node15);
+        node15.setRight(node17);
+        node15.setLeft(node13);
+        node13.setRight(node12);
+        node13.setLeft(node14);
+        node17.setRight(node18);
+        node17.setLeft(node16);
+
+        assertTrue(checkLengthOfBranch(root));
+    }
+
+        class RBNodeTest<T> implements RBNode<T> {
 
         private T value;
         private RBNode<T> left = null;
